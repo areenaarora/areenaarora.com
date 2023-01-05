cce5f99cb980d94c5bd8dab289d6fdd6b68e847c
Areena Arora <areena.arora@gmail.com>
2023-01-05 12:10:14 -0500

    Added plausible

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,15 @@
 <script>
+	import { dev } from '$app/environment';
 	import Footer from '../lib/Footer.svelte';
 	import Header from '../lib/Header.svelte';
 	import '../app.css';
 </script>
+
+<svelte:head>
+	{#if !dev}
+		<script defer data-domain="areenaarora.com" src="https://plausible.io/js/script.js"></script>
+	{/if}
+</svelte:head>
 
 <Header />
 <main>
