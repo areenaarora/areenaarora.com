3ca9cdea61a70d065f0e97ab697cf37d5bfe6c5f
Areena Arora <areena.arora@gmail.com>
2023-01-05 00:31:27 -0500

    Initial commit

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,67 @@
+<body>
+    <div class="footer">
+        <a href="#Home" class="Home">Areena Arora</a>
+        <div class="header-right">
+          <a href="#portfolio">Journalism</a>
+          <a href="#about">About</a>
+          <a href="#projects">Personal Projects</a>
+        </div>
+      </div>
+    </body>
+    <slot />
+    
+    <style>
+    
+    body { 
+        margin: 0;
+        font-family:sans-serif;
+        }
+      
+      .header {
+        overflow: hidden;
+        background-color:floralwhite;
+        padding: 20px 10px;
+      }
+      
+      .header a {
+        float: left;
+        color: black;
+        text-align: center;
+        padding: 12px;
+        text-decoration: none;
+        font-size: 18px; 
+        line-height: 25px;
+        border-radius: 4px;
+      }
+      
+      .header a.Home {
+        font-size: 25px;
+        font-weight: bold;
+      }
+      
+      .header a:hover {
+        background-color:gold;
+        color: black;
+      }
+      
+      .header a.active {
+        background-color: dodgerblue;
+        color: white;
+      }
+      
+      .header-right {
+        float: right;
+      }
+      
+      @media screen and (max-width: 500px) {
+        .header a {
+          float: none;
+          display: block;
+          text-align: left;
+        }
+        
+        .header-right {
+          float: none;
+        }
+      }
+      </style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,49 @@
+<script>
+	import Footer from '../lib/Footer.svelte';
+	import Header from '../lib/Header.svelte';
+</script>
+
+<Header />
+
+<body>
+	<div class="center-content">
+		<h1>Hi! I'm Areena 👋🏻</h1>
+		<p>
+			I'm a data journalist, <a href="https://www.instagram.com/feelin_gredient/"
+				>occassional food blogger,</a
+			> and house plant enthusiast!
+		</p>
+		<p>
+			I'm new to the world of data-powered storytelling, and slowly paving my path at <a
+				href="https://www.knoxnews.com/staff/8034861001/areena-arora/"
+				target="_blank"
+				rel="noreferrer"
+			>
+				Knox News.</a
+			>
+		</p>
+		<p>
+			I'm also ✨NEW✨ to coding, and learning Svelte. This website is where I hope to showcase my
+			best front-end work.
+		</p>
+	</div>
+
+	<Footer />
+</body>
+
+<style>
+	.center-content {
+		margin: 0px;
+		padding: 50px 350px;
+	}
+
+	h1 {
+		font-family: sans-serif;
+		font-size: 60px;
+		color: seagreen;
+	}
+
+	p {
+		font-size: 20px;
+	}
+</style>
