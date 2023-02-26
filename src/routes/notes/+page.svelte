10a272834ec4ac19c746add22792ef7dacf7860f
Areena Arora <areena.arora@gmail.com>
2023-02-25 20:03:28 -0500

    Updated notes page

--- src/routes/notes/+page.svelte
+++ src/routes/notes/+page.svelte
@@ -6,15 +6,11 @@
 <title>Notes and musings</title>
 
 <div class="center-content">
-	<h3>2023 is the year of rage reading and visualizing history — specifically Indian history!</h3>
+	<h3>
+		2023 is the year of visualizing history and government documents — specifically about India!
+	</h3>
 
-	<p>This is a work in progress!</p>
-	<p>
-		In the next week or so, I hope to make this table better styled, searchable and I want to be
-		able to update it dynamically from a csv or json file. I will also be visualising another
-		dataset about the 292 people who wrote the Indian constitution!
-	</p>
+	<p>🚧🚧This page is a work in progress!🚧🚧</p>
 	<p>Stay tuned!</p>
-	<Table />
-	<p>Updated: January 18, 2023</p>
+	<p>Updated: February 25, 2023</p>
 </div>
