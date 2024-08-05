944af6133bd6fd085728326b05372ef1c96e665e
Areena Arora <areena.arora@gmail.com>
2024-08-04 22:04:58 -0400

    Update +page.svelte

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -1,11 +1,9 @@
 <title>Experiments</title>
 
 <div class="center-content">
-	<h3>
-		2023 is the year of visualizing history and government documents — specifically about India!
-	</h3>
+	<h3>Bare with me as I figure this space out🌻</h3>
 
 	<h3>🚧 This page is a work in progress! 🚧</h3>
 	<p>Stay tuned!</p>
-	<p>Last updated: July 30, 2023</p>
+	<p>Last updated: August 4, 2024</p>
 </div>
