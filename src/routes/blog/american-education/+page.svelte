dd72a33e88b01b029a28707589b25c450270792e
Areena Arora <areena.arora@gmail.com>
2025-02-14 11:20:43 -0500

    Updated blog

--- src/routes/blog/american-education/+page.svelte
+++ src/routes/blog/american-education/+page.svelte
@@ -3,124 +3,157 @@
 	<h1>On American public education</h1>
 	<p>
 		Americans care about public education in a way I hadn't fully comprehended 2½ years ago.
-		Americans also politicize education to an extent I couldn't have anticipated. My husband often
-		remarks I write almost equal parts about policies and politics as I do about the core tenets of
-		education − curriculum, testing and literacy.
+		Americans also politicize education to an extent I couldn't have anticipated.
 	</p>
+
+	<p>
+		My husband often remarked I wrote almost equal parts about policies and politics as I did about
+		the core tenet and purpose of education – literacy.
+	</p>
+
 	<p>
 		I grew up in India and I hadn't been to an American public school before I was assigned to the
-		Knox News children's issues beat. Now, as I'm gearing up to move back home − Feb. 7 was my last
+		Knox News’ public education beat. Now, as I'm gearing up to move back home − Feb. 7 was my last
 		day at Knox News − I'm reflecting on what has surprised me most.
 	</p>
+
 	<p>
 		There is no national curriculum here. Common Core standards come close, but those too aren't
-		mandated and have not been universally liked or accepted. State standards differ and local
-		communities choose their academic priorities. In just my short time here, though, I've seen the
-		lines between local priorities and national political agendas blur.
+		mandated and have not been <a
+			href="https://www.k12dive.com/news/report-us-reading-math-scores-drop-after-common-core-implementation/576972/"
+			target="_blank">universally liked or accepted</a
+		>. There is also research showing how math and reading scores dropped after Common Core was
+		implemented.
 	</p>
+
+	<p>
+		State standards differ and local communities choose their academic priorities. But, in just my
+		short time here, though, I've seen the lines between <a
+			href="https://www.knoxnews.com/story/news/politics/2025/01/29/knox-county-school-board-chair-testifies-for-private-school-vouchers/78020949007/"
+			target="_blank">local priorities</a
+		>
+		and
+		<a
+			href="https://www.knoxnews.com/story/news/education/2023/05/08/knox-county-school-board-routine-vote-echoes-national-partisan-divide/70172398007/"
+			target="_blank">national political agendas</a
+		> blur.
+	</p>
+
 	<p>
 		Curriculum in India is a lot more consistent across the country, for private and public schools
-		alike. Religion doesn't enter the equation and isn't at odds with literacy and scientific
-		discovery. Faith is to be practiced at home.
+		alike.
 	</p>
+
+	<p>
+		For one, religion mostly <a
+			href="https://slate.com/life/2024/10/lifewise-academy-legality-ohio-parents-school-board.html"
+			target="_blank">doesn't enter the equation</a
+		> and curriculum in India is generally not at a crossroads between religion and scientific discovery.
+		Faith is to be practiced at home.
+	</p>
+
 	<p>
 		When I first heard Tennessee's standard on sex education is abstinence, I remember thinking
-		"surely, there's no way that could be true." It goes against the idea to which I'd long
-		subscribed of a progressive, first world country.
+		"surely, there's no way that could be true." It goes against the idea of a progress, first world
+		country to which I'd long subscribed to.
 	</p>
+
 	<p>
-		Censorship of books was another topic I never thought I'd be writing about in the land of free
-		and brave ideas. While I see value in ensuring kids are exposed to age-appropriate content, the
-		wording of recent legislation is disingenuous in that regard. It instills fear in trained,
+		<a
+			href="https://www.knoxnews.com/story/news/education/2025/01/21/how-knox-county-schools-decided-which-books-to-remove-from-libraries/77048502007/"
+			target="_blank"
+			>Censorship of books
+		</a> was another topic I never thought I'd be writing about in the land of free and brave ideas.
+	</p>
+
+	<p>
+		While I see some value in ensuring kids are exposed to age-appropriate content, the wording of
+		recent legislation in the state is disingenuous in that regard. It instills fear in trained,
 		professional educators, rather than promoting healthy conversations around what's appropriate
 		and what isn't.
 	</p>
-	<p>There are, of course, ideas I want to steal and take back home with me.</p>
+
+	<p>
+		At a time when fourth and eighth graders still aren’t back to performing at pre-pandemic 2019
+		levels, the priority and focus shouldn’t be, dare I say, censorship.
+	</p>
+
+	<p>
+		All’s not doom and gloom and there are, of course, ideas I want to steal and take back home with
+		me.
+	</p>
+
 	<p>
 		The teacher-student relationship in India is strictly academic, and teachers' involvement in
 		students' lives is limited. Here, I see teachers devote themselves to not just educating, but
 		also mentoring and going far above and beyond their prescribed duties.
 	</p>
+
 	<p>
 		When I see teachers here buying supplies for their classrooms and advocating before lawmakers
 		for their schools, I'm envious of their commitment to public education. That said, I'm also
 		confused by the lack of resources available to support public education that warrants this
 		advocacy in the first place.
 	</p>
+
 	<p>
 		Public education in India is seen as a last-resort option, reserved for those who need it. Here,
-		it's the backbone of the society.
+		it is believed to be the backbone of the society.
 	</p>
 
 	<p>
-		It has therefore puzzled me to see lawmakers rallying behind directing funds to private schools,
-		draining public schools. There are 900,000 public school students in the state and by my
-		calculation, split evenly, the $447 million voucher program could have meant nearly $500 more
-		for every student enrolled in a public school in the state.
+		It's an interesting time for public education, given the <a
+			href="https://www.theguardian.com/us-news/2025/feb/11/doge-education-department-cuts-trump"
+			target="_blank">Donald Trump-Elon Musk administration</a
+		>'s push to consider dismantling the U.S. Department of Education.
 	</p>
+
 	<p>
-		It's an interesting time for public education, given the Donald Trump administration's push to
-		consider dismantling the U.S. Department of Education. The agency's funds give an extra boost to
-		schools with the highest levels of poverty. Those dollars could pay for social workers,
-		behavioral interventionists and coaches. With states like Tennessee funneling away funds to
-		private schools, taking away the source of Title I funds seems unwise.
+		The agency's funds give an <a
+			href="https://www.knoxnews.com/story/news/education/2024/07/31/knox-county-schools-alter-staffing-levels-with-federal-funding-changes/73587268007/"
+			target="_blank">extra boost</a
+		> to schools with the highest levels of poverty. Those dollars pay for social workers, behavioral
+		interventionists and coaches.
 	</p>
+
 	<p>
 		To this foreigner, focus should instead be on streamlining curriculum so it's more future-proof,
 		especially when literacy skills are dipping to all-time lows, painting a bleak picture of
-		education in this country. Data in the latest National Report Card showed student performance
-		worsened in math and reading in 2024 testing compared to before the COVID-19 pandemic in 2019.
+		education in this country.
 	</p>
+
 	<p>
-		Finally, there's special education − a topic that's been the subject of many of my stories
-		covering Knox County Schools. The laws in India protecting individuals with disabilities are
-		bleak at best and shamefully lacking compared to the protections afforded to Americans under
-		Individuals with Disabilities Education Act, yet often, as I've reported, legal protections
-		aren't enough.
+		Finally, there's <a
+			href="https://www.knoxnews.com/story/news/education/2024/06/06/special-education-worries-force-knox-county-family-to-homeschool/73904913007/"
+			target="_blank">special education</a
+		> − a topic that's been the subject of many of my stories covering Knox County Schools.
 	</p>
+
 	<p>
-		I've seen inspiring moms advocate for their kids in special education, but they're faced with −
-		to borrow words from the county's special education task force − "a culture of resistance" and
-		retaliation. After decades of parents fighting for special education reform, the district
-		announced a task force in fall of 2023 and made a public resolve to make Knox County a "model
-		district for excellence in special education," but the task force hasn't met in months and at
-		least one family I know has turned to homeschooling instead. One too many to be making that hard
-		decision.
+		The laws in India protecting individuals with disabilities are bleak at best and shamefully
+		lacking compared to the protections afforded to Americans under Individuals with Disabilities
+		Education Act, yet often, as I've reported, <a
+			href="https://www.knoxnews.com/story/opinion/columnists/2024/12/23/knox-county-schools-special-education-task-force-was-never-about-real-reform-victor-ashe-opinion/77026568007/"
+			target="_blank"
+			>legal protections aren't enough
+		</a>.
 	</p>
+
 	<p>
-		Here's another interesting one. Last year, I also wrote about concerns surrounding school safety
-		and whether Knox County Schools should allow teachers and staff to carry guns, following the
-		passing of a Tennessee law allowing teachers and staffers to carry concealed weapons in
-		classrooms. My dad was in the Indian military and I grew up going to military schools and yet, I
-		could never understand even needing a discussion on arming teachers.
+		I've seen inspiring moms advocate for their kids in special education, but they're faced with a
+		culture of <a
+			href="https://www.knoxnews.com/story/news/education/2023/08/08/task-force-review-finds-45-deficiencies-in-districts-special-education-program/70532403007/"
+			target="_blank">resistance and retaliation</a
+		>.
 	</p>
-	<p>And then there are smaller differences.</p>
+
 	<p>
-		In India, schools have uniforms. In the U.S., where public education is considered the "great
-		equalizer," I was often disheartened when I saw the income gap among students on full display.
-		Some students wear expensive sneakers and clothing, others are in need of assistance from the
-		clothing center. Back home, we all wore the same uniform and all looked the same in school.
+		To the parents and teachers who have trusted me with their stories, thank you − for your faith
+		in me and for your patience as I navigated the complex world of American public education as an
+		outsider.
 	</p>
+
 	<p>
-		Being a reporter can sometimes hardwire your brain to see only the worst in everything. There is
-		great pride in asking difficult questions and on most days, that means writing and highlighting
-		wrongs in the world, but we try, too, to be present when our community celebrates.
-	</p>
-	<p>
-		In my time here, I've seen students and teachers inspire with their sharp wit and innovative
-		thinking. I've written about a foster grandparents program bringing extra hugs to students and
-		about teachers at Northwest Middle School learning Spanish to better support their newest
-		students who don't speak English.
-	</p>
-	<p>
-		I've seen admirable resilience from communities who will be rebuilding their schools for years
-		after the devasting Hurricane Helene. It has been truly rewarding, and hopefully someday (soon)
-		I'll write a book about it.
-	</p>
-	<p>
-		Until then, I'll be watching keenly what the future holds for public education in the U.S. and
-		of course more closely for Knox County and East Tennessee. To the parents and teachers who have
-		trusted me with their stories, thank you − for your faith in me and for your patience as I
-		navigated the complex world of American public education as an outsider.
+		If you've enjoyed my work and have feedback − good or bad − for me, I'd love to hear from you!
 	</p>
 </div>
