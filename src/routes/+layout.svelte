97c30c7fa6cddf48c91a363c752768aa01736105
Areena Arora <areena.arora@gmail.com>
2025-09-13 13:40:27 +0530

    add plausible

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,19 @@
 <script>
-	import "../styles.css";
-	import Header from "$lib/components/Header.svelte";
+	import '../styles.css';
+	import Header from '$lib/components/Header.svelte';
+	import { dev } from '$app/environment';
 </script>
 
+<svelte:head>
+	{#if !dev}
+		<script
+			data-api="/stats/api/event"
+			data-domain="areenaarora.com"
+			src="/stats/js/script.js"
+		></script>
+	{/if}
+</svelte:head>
+
 <Header />
+
 <slot />
